f2ec7d1b6c76b18e51bd8eb69457bca44c0ade8c
Tomoki Kobayashi <temoki@gmail.com>
2024-07-06 13:13:39 +0900

    create new xcode project

---
+++ SwiftMvGql/SwiftMvGqlApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftMvGqlApp.swift
+//  SwiftMvGql
+//
+//  Created by Tomoki Kobayashi on 2024/07/06.
+//
+
+import SwiftUI
+
+@main
+struct SwiftMvGqlApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
